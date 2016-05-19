d9c62d3b67c65042c685b2758339ba113221753b
Zak Bowyer <zak.bowyer@live.co.uk>
2016-05-19 15:55:32 +0100

    angular blog prep

--- website/src/main/java/com/website/controller/HomeController.java
+++ website/src/main/java/com/website/controller/HomeController.java
@@ -1,24 +1,24 @@
 package com.website.controller;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.website.model.BlogPost;
-
+@SpringBootApplication
 @RestController
 public class HomeController {
 	
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
-	
-	@RequestMapping(value="blog", method = RequestMethod.GET)
-	public List<BlogPost> list() {
-		return BlogPostStub.list();
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
 	}
 	
 }
--- website/src/main/resources/public/js/app.js
+++ website/src/main/resources/public/js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module("app", []);
+angular.module('app', ['ngRoute'])
+	.config(function($routeProvider, $httpProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'home',
+			controllerAs: 'controller'
+		}).when('/blog', {
+			templateUrl: 'blog.html',
+			controller: 'blog',
+			controllerAs: 'controller'
+		}).otherwise('/');
+		
+		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	})
+	.controller('home', function($http) {
+		var self = this;
+		$http.get('/resource/').then(function(response) {
+			console.log(response);
+			self.greeting = response.data;
+		})
+	})
+	.controller('blog', function($location) {
+		$location.path("/blog");
+	});
