f4176cf885b78ef69a8afd38c6cbc31e4409d500
Zak Bowyer <zak.bowyer@live.co.uk>
2016-06-27 14:29:05 +0100

    move scroll behaviour to new file

--- website/src/main/resources/public/views/viewstory/viewstory.controller.js
+++ website/src/main/resources/public/views/viewstory/viewstory.controller.js
@@ -3,38 +3,7 @@
 	
 	angular
 		.module('bowyerville')
-		.controller('ViewStoryController', ViewStoryController)
-		.directive('scrolly', function() {
-			return {
-				restrict: 'A',
-				link: function(scope, element, attrs) {
-					var raw = element[0];
-					scope.selected.scrollPositions = {
-						start: 0
-					};
-					element.bind("scroll", function() {
-						scope.selected.scrollPositions = {
-							start: scope.selected.scrollPositions.start == null ? 0 : scope.selected.scrollPositions.start,
-							end: scope.selected.scrollPositions.end == null ? raw.scrollHeight : scope.selected.scrollPositions.end
-						}
-						var currentPosition = raw.scrollTop + raw.offsetHeight;
-						if (currentPosition < scope.selected.scrollPositions.start) {
-							scope.$apply(attrs.last);
-						}
-						else if (currentPosition >= scope.selected.scrollPositions.end) {
-							var nextStartPosition = scope.selected.scrollPositions.end;
-							scope.$apply(attrs.scrolly);
-							if (scope.selected.scrollPositions.start == null) {
-								scope.selected.scrollPositions = {
-									start: nextStartPosition,
-									end: raw.scrollEnd
-								}
-							}
-						}
-					});
-				}
-			}
-		});
+		.controller('ViewStoryController', ViewStoryController);
 	
 	
 	/** @ngInject */
