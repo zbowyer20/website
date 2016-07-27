8c33caddec26aebc2c4d15d3de988f1ef3be4e12
Zak Bowyer <zak.bowyer@live.co.uk>
2016-07-27 14:12:26 +0100

    code restructure

--- website/src/main/resources/public/views/viewstory/viewstory.controller.js
+++ website/src/main/resources/public/views/viewstory/viewstory.controller.js
@@ -63,26 +63,18 @@
 			}
 		};
 		
-		loading();
-
-		// pick up all available stories
-		$http.get('php/services/getStories.php').then(function(response) {
-			data = response.data;
-			$scope.init();
-			// TODO implement first story
-			$scope.showContent($scope.getStoryByFileName($scope.settings.latestStoryViewed || "albert-1"));
-			updateCookie($scope.content.selected);
-			$scope.content.selected.visible = true;
-			stopLoading();
-		});
-		
-		$scope.init = function() {
-			$scope.refresh();
+		/*
+		 * Initialise stories
+		 */
+		function init() {
+			refresh();
+			// hide all roundels
 			for (var i = 0; i < data.length; i++) {
 				data[i].roundel = {
 					x: null
 				};
 			}
+			// add all previously seen cookies to the timeline
 			for (var i = 0; i < data.length; i++) {
 				if (storyIsInCookie(data[i])) {
 					addStoryToTimeline(data[i]);
@@ -90,74 +82,13 @@
 			}
 		}
 		
-		function storyIsInTimeline(story) {
-			return $scope.content.stories.indexOf(story) > -1;
-		}
-		
-		function addStoryToTimeline(story) {
-			if (!storyIsInTimeline(story)) {
-				$scope.content.stories.push(story);
-				$scope.content.stories.sort(function(a, b) {
-					return new Date(a.timeSetting) - new Date(b.timeSetting);
-				});
-				$scope.setRoundelLocations(story);
-			}
-		}
-			
-		// clear up all content in text box
-		$scope.refresh = function() {
+		/*
+		 * Clear up all content in text box
+		 */
+		function refresh() {
 			$scope.content.text = "";
 			for (var i = 0; i < data.length; i++) {
 				data[i].scrollPositions = {};
-			}
-		}
-			
-		// display new content
-		$scope.showContent = function(content, refresh) {
-			// if content is to be cleared
-			if (refresh) {
-				console.log("Showing refreshed story: ");
-				console.log(content);
-				$scope.refresh();
-				// TODO update angularly
-				$(".article__content").scrollTop(0);
-				$scope.settings.interactedWithTimeline = true;
-				updateCookie(content);
-			}
-			addStoryToTimeline(content);
-			$scope.loadContent(content);
-			$scope.content.selected = content;
-			updateLatestDate(content.timeSetting);
-			
-			// update the visible image
-			$scope.content.images.current = $scope.content.images.current === 0 ? 1 : 0;
-			$scope.content.images.container[$scope.content.images.current] = content.img;
-			
-			// update playing video, if necessary
-			$scope.content.video.id = content.youtubeId || null;
-			if ($scope.youtube != null && $scope.content.video.id == null && $scope.youtube.currentState == "playing") {
-				$scope.youtube.stopVideo();
-			}
-			
-			if (newStoryIsAvailable()) {
-				addStoryToTimeline($scope.getStoryByFileName($scope.content.selected.next));
-			}
-		}
-		
-		// display story content, loading it from backend if necessary
-		$scope.loadContent = function(story) {
-			// only load from backend if it hasn't already been loaded
-			if (typeof story.content == 'undefined' && story.type != 'video') {
-				loading();
-				$http.get('html/' + story.fileName + '.html').then(function(response) {
-					stopLoading();
-					story.content = response.data;
-					$scope.content.text += story.content;
-					$cookieStore.put("nextStory", story.next);
-				})
-			}
-			else if (story.scrollPositions.start == null) {
-				$scope.content.text += story.content;
 			}
 		}
 		
@@ -176,57 +107,88 @@
 			$scope.settings.loading.on = false;
 		}
 		
-		$scope.isSelected = function(title) {
-			return $scope.content.selected.title == title;
-		}
-				
-		$scope.getRoundelLocation = function(dateStr) {
-			var currentProportion = 0;
-			var date = new Date(dateStr);
-			for (var i = 0; i < $scope.TIME_PERIODS.length; i++) {
-				if (date > $scope.TIME_PERIODS[i].start && date < $scope.TIME_PERIODS[i].end) {
-					var timeIntoPeriod = date - $scope.TIME_PERIODS[i].start;
-					var periodTotal = $scope.TIME_PERIODS[i].end - $scope.TIME_PERIODS[i].start;
-					var timeProportion = (timeIntoPeriod / periodTotal) * 100;
-					var totalTimePeriodProportion = ($scope.TIME_PERIODS[i].timeline - currentProportion) / 100;
-					var storyDate = (timeProportion * totalTimePeriodProportion) + currentProportion;
-					return storyDate;
-				}
-				currentProportion = $scope.TIME_PERIODS[i].timeline;
+		function getStoryByFileName(fileName) {
+			for (var i = 0; i < data.length; i++) {
+				if (data[i].fileName == fileName) return data[i];
 			}
 		}
 		
-		// set the timeline location for a story
-		$scope.setRoundelLocations = function(story) {
-			if (story.roundel.x == null) {
-				story.roundel.x = $scope.getRoundelLocation(story.timeSetting);
-			}
-			reposition();
-		}
-		
-		$scope.roundelIsVisible = function(story) {
-			return story.fileName == $scope.content.selected.fileName || storyIsInCookie(story) || 
-					newStoryIsAvailable();
-		}
-		
-		$scope.storyIsNew = function(story) {
-			return (!storyIsInCookie(story) && $scope.roundelIsVisible(story));
-		}
-		
-		// gather and sort visible roundels, then reposition them on timeline
-		function reposition() {
-			for (var i = 0; i < $scope.content.stories.length - 1; i++) {
-				var distance = $scope.content.stories[i + 1].roundel.x - $scope.content.stories[i].roundel.x;
-				if (distance < 2) {
-					$scope.content.stories[i + 1].roundel.x += 2 - distance;
-				}
+		/*
+		 * Display new content
+		 */
+		function updateContent(story) {
+			addStoryToTimeline(story);
+			$scope.content.selected = story;
+			displayContent(story);
+			
+			if (newStoryIsAvailable()) {
+				addStoryToTimeline(getStoryByFileName(story.next));
 			}
 		}
 		
+		/*
+		 * Load and show text, image and video relating to a story
+		 */
+		function displayContent(story) {
+			// only load from backend if it hasn't already been loaded
+			if (typeof story.content == 'undefined' && story.type != 'video') {
+				loading();
+				$http.get('html/' + story.fileName + '.html').then(function(response) {
+					stopLoading();
+					story.content = response.data;
+					$scope.content.text += story.content;
+					$cookieStore.put("nextStory", story.next);
+				})
+			}
+			else if (story.scrollPositions.start == null) {
+				$scope.content.text += story.content;
+			}
+			updateLatestDate(story.timeSetting);
+			updateImage(story.img);
+			updateVideo(story.youtubeId);
+		}
+
 		function updateLatestDate(dateStr) {
 			var date = new Date(dateStr);
 			if (date > $scope.settings.latestDate) {
 				$scope.settings.latestDate = date;
+			}
+		}
+		
+		function updateImage(image) {
+			$scope.content.images.current = $scope.content.images.current ? 0 : 1;
+			$scope.content.images.container[$scope.content.images.current] = image;
+		}
+		
+		function updateVideo(id) {
+			$scope.content.video.id = id || null;
+			cleanVideo(0);
+			cleanVideo(500);
+		}
+		
+		function cleanVideo(timeout) {
+			$timeout(function() {
+				if (unexpectedVideoIsPlaying()) {
+					$scope.youtube.stopVideo();
+				}
+			}, timeout);
+		}
+		
+		function unexpectedVideoIsPlaying() {
+			return $scope.youtube != null && $scope.content.video.id == null && $scope.youtube.currentState == "playing";
+		}
+		
+		function storyIsInTimeline(story) {
+			return $scope.content.stories.indexOf(story) > -1;
+		}
+		
+		function addStoryToTimeline(story) {
+			if (!storyIsInTimeline(story)) {
+				$scope.content.stories.push(story);
+				$scope.content.stories.sort(function(a, b) {
+					return new Date(a.timeSetting) - new Date(b.timeSetting);
+				});
+				setRoundelLocations(story);
 			}
 		}
 		
@@ -246,6 +208,71 @@
 			return $scope.settings.latestStoryViewed == $scope.content.selected.fileName && $scope.content.selected.next != "" && "" == $scope.settings.cookieNextStory;
 		}
 		
+		function getRoundelLocation(dateStr) {
+			var currentProportion = 0;
+			var date = new Date(dateStr);
+			for (var i = 0; i < $scope.TIME_PERIODS.length; i++) {
+				if (date > $scope.TIME_PERIODS[i].start && date < $scope.TIME_PERIODS[i].end) {
+					var timeIntoPeriod = date - $scope.TIME_PERIODS[i].start;
+					var periodTotal = $scope.TIME_PERIODS[i].end - $scope.TIME_PERIODS[i].start;
+					var timeProportion = (timeIntoPeriod / periodTotal) * 100;
+					var totalTimePeriodProportion = ($scope.TIME_PERIODS[i].timeline - currentProportion) / 100;
+					var storyDate = (timeProportion * totalTimePeriodProportion) + currentProportion;
+					return storyDate;
+				}
+				currentProportion = $scope.TIME_PERIODS[i].timeline;
+			}
+		}
+		
+		// set the timeline location for a story
+		function setRoundelLocations(story) {
+			if (story.roundel.x == null) {
+				story.roundel.x = getRoundelLocation(story.timeSetting);
+			}
+			reposition();
+		}
+		
+		// gather and sort visible roundels, then reposition them on timeline
+		function reposition() {
+			for (var i = 0; i < $scope.content.stories.length - 1; i++) {
+				var distance = $scope.content.stories[i + 1].roundel.x - $scope.content.stories[i].roundel.x;
+				if (distance < 2) {
+					$scope.content.stories[i + 1].roundel.x += 2 - distance;
+				}
+			}
+		}
+		
+		function updateMute(muted) {
+			$scope.settings.muted = muted;
+			$cookieStore.put("muted", muted);
+		}
+		
+		/*
+		 * Refresh the story and jump to a piece of content
+		 */
+		$scope.goToContent = function(story) {
+			console.log("Showing refreshed story: ");
+			console.log(story);
+			refresh();
+			$(".article__content").scrollTop(0);
+			$scope.settings.interactedWithTimeline = true;
+			updateCookie(story);
+			updateContent(story);
+		}
+		
+		$scope.isSelected = function(title) {
+			return $scope.content.selected.title == title;
+		}
+		
+		$scope.roundelIsVisible = function(story) {
+			return story.fileName == $scope.content.selected.fileName || storyIsInCookie(story) || 
+					newStoryIsAvailable();
+		}
+		
+		$scope.storyIsNew = function(story) {
+			return (!storyIsInCookie(story) && $scope.roundelIsVisible(story));
+		}
+		
 		$scope.isImageActive = function() {
 			return $scope.content.images.container[$scope.content.images.current] != null;
 		}
@@ -256,15 +283,9 @@
 			return new Date($scope.content.selected.timeSetting) > date;
 		}
 		
-		$scope.hasPassed = function(dateStr) {
+		$scope.dateIsLessThanLatestDate = function(dateStr) {
 			var date = new Date(dateStr);
 			return date <= $scope.settings.latestDate;
-		}
-		
-		$scope.getStoryByFileName = function(fileName) {
-			for (var i = 0; i < data.length; i++) {
-				if (data[i].fileName == fileName) return data[i];
-			}
 		}
 		
 		$scope.getTooltipText = function(story) {
@@ -274,11 +295,11 @@
 		// show the next story, eg. after scrolling to end of current story
 		$scope.getNext = function(refresh) {
 			if ($scope.content.selected.next != "") {
-				var story = $scope.content.selected.scrollNext == null ? $scope.getStoryByFileName($scope.content.selected.next) : $scope.content.selected.scrollNext;
+				var story = $scope.content.selected.scrollNext == null ? getStoryByFileName($scope.content.selected.next) : $scope.content.selected.scrollNext;
 				if (story != null) {
 					story.scrollPrev = $scope.content.selected;
 					updateCookie(story);
-					$scope.showContent(story, refresh);
+					refresh ? $scope.goToContent(story) : updateContent(story);
 				}
 			}
 			else {
@@ -288,13 +309,8 @@
 		
 		// get the previous story, eg. scrolling back to the top of current story
 		$scope.getLast = function() {
-			$scope.showContent($scope.content.selected.scrollPrev);
+			updateContent($scope.content.selected.scrollPrev);
 			$scope.content.teaser = "";
-		}
-		
-		function updateMute(muted) {
-			$scope.settings.muted = muted;
-			$cookieStore.put("muted", muted);
 		}
 		
 		// toggle between mute and volume for currently playing youtube video
@@ -319,6 +335,19 @@
 				player.playVideo();
 			}
 		})
+		
+		loading();
+
+		// pick up all available stories
+		$http.get('php/services/getStories.php').then(function(response) {
+			data = response.data;
+			init();
+			// TODO implement first story
+			updateContent(getStoryByFileName($scope.settings.latestStoryViewed || "albert-1"));
+			updateCookie($scope.content.selected);
+			$scope.content.selected.visible = true;
+			stopLoading();
+		});
 					
 	}
 })();
