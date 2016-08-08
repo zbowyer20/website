35851d09bb4911ab6edf134d6c1008c907324a66
Zak Bowyer <zak.bowyer@live.co.uk>
2016-08-08 14:48:06 +0100

    character popup

--- website/src/main/resources/public/js/bundle.js
+++ website/src/main/resources/public/js/bundle.js
@@ -87340,12 +87340,25 @@ return jQuery;
 	/** @ngInject */
 	function CharactersController($http, $scope) {
 		$scope.characters = [];
-
+		$scope.selected = null;
+		
 		// pick up all available stories
 		$http.get('php/services/getCharacters.php').then(function(response) {
 			console.log(response);
 			$scope.characters = response.data;
 		});
+		
+		$scope.showCharacter = function(character) {
+			$scope.selected = character;
+			console.log("Show character");
+			if (typeof (character.content) == "undefined") {
+				console.log("undefined");
+				$http.get(character.fileName).then(function(response) {
+					console.log(response);
+					$scope.selected.content = response.data;
+				});
+			}
+		}
 					
 	}
 })();
--- website/src/main/resources/public/views/characters/characters.controller.js
+++ website/src/main/resources/public/views/characters/characters.controller.js
@@ -9,12 +9,25 @@
 	/** @ngInject */
 	function CharactersController($http, $scope) {
 		$scope.characters = [];
-
+		$scope.selected = null;
+		
 		// pick up all available stories
 		$http.get('php/services/getCharacters.php').then(function(response) {
 			console.log(response);
 			$scope.characters = response.data;
 		});
+		
+		$scope.showCharacter = function(character) {
+			$scope.selected = character;
+			console.log("Show character");
+			if (typeof (character.content) == "undefined") {
+				console.log("undefined");
+				$http.get(character.fileName).then(function(response) {
+					console.log(response);
+					$scope.selected.content = response.data;
+				});
+			}
+		}
 					
 	}
 })();
