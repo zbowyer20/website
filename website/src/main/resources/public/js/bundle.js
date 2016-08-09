1afe61dfa90d7a1bef898a7a2df7a66036472bd1
Zak Bowyer <zak.bowyer@live.co.uk>
2016-08-09 14:20:49 +0100

    characters design 2

--- website/src/main/resources/public/js/bundle.js
+++ website/src/main/resources/public/js/bundle.js
@@ -87365,6 +87365,15 @@ return jQuery;
 		$scope.stripGroup = function(str) {
 			return str.replace(/[\s:]/g, "");
 		}
+		
+		$scope.getLeft = function(index, group) {
+			console.log(index);
+			console.log(group);
+			//return "calc(50% + 10px)";
+			return "calc(" + (50 - (group.length / 8) * 50) + "% + " + (75 * index) + "px)";
+			//return "calc(" + ((1 - (group.length/8)) * 100) + "% + " + (70*index) + "px)");
+			//return ((index / group.length) * 100) + "%";
+		}
 					
 	}
 })();
