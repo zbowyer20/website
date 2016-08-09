52af7a17fc6afc718f59af6879deaf2bd3ba1273
Zak Bowyer <zak.bowyer@live.co.uk>
2016-08-09 15:14:33 +0100

    redesigned character bio page

--- website/src/main/resources/public/js/bundle.js
+++ website/src/main/resources/public/js/bundle.js
@@ -87367,12 +87367,7 @@ return jQuery;
 		}
 		
 		$scope.getLeft = function(index, group) {
-			console.log(index);
-			console.log(group);
-			//return "calc(50% + 10px)";
 			return "calc(" + (50 - (group.length / 8) * 50) + "% + " + (75 * index) + "px)";
-			//return "calc(" + ((1 - (group.length/8)) * 100) + "% + " + (70*index) + "px)");
-			//return ((index / group.length) * 100) + "%";
 		}
 					
 	}
