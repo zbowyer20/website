31a0e4894fcac3bba34642977156129783ea3ddc
Zak Bowyer <zak.bowyer@live.co.uk>
2016-08-11 11:09:09 +0100

    load subheaders in js

--- website/src/main/resources/public/js/bundle.js
+++ website/src/main/resources/public/js/bundle.js
@@ -87419,7 +87419,29 @@ angular.module('bowyerville')
 	
 	/** @ngInject */
 	function HomeController($http, $scope, $cookieStore) {
-					
+		$scope.subheaders = [
+		    {
+		    	href: "#/developer",
+		    	todo: false,
+		    	text: "DEVELOPER"
+		    },
+		    {
+		    	href: "#/writer",
+		    	todo: false,
+		    	text: "WRITER"
+		    }
+		];
+		var todoSubheaders = ["OLYMPIAN", "ADVENTURER", "SPACEMAN", "WIZARD", "HUNK"];
+		
+		function addSubheader() {
+			var rand = Math.floor(Math.random() * todoSubheaders.length);
+			$scope.subheaders.push({
+				todo: true,
+				text: todoSubheaders[rand]
+			});
+		}
+		
+		addSubheader();
 	}
 })();
 
--- website/src/main/resources/public/views/home/home.controller.js
+++ website/src/main/resources/public/views/home/home.controller.js
@@ -8,6 +8,28 @@
 	
 	/** @ngInject */
 	function HomeController($http, $scope, $cookieStore) {
-					
+		$scope.subheaders = [
+		    {
+		    	href: "#/developer",
+		    	todo: false,
+		    	text: "DEVELOPER"
+		    },
+		    {
+		    	href: "#/writer",
+		    	todo: false,
+		    	text: "WRITER"
+		    }
+		];
+		var todoSubheaders = ["OLYMPIAN", "ADVENTURER", "SPACEMAN", "WIZARD", "HUNK"];
+		
+		function addSubheader() {
+			var rand = Math.floor(Math.random() * todoSubheaders.length);
+			$scope.subheaders.push({
+				todo: true,
+				text: todoSubheaders[rand]
+			});
+		}
+		
+		addSubheader();
 	}
 })();
